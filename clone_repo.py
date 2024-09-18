629ad123daf031690c6a67d40d2bd92bd7a8b048
B Venkatesh Reddy <bvenkateshreddy87@gmail.com>
2024-09-18 14:53:46 +0530

    Add files via upload

---
+++ clone_repo.py
@@ -0,0 +1,21 @@
+from git import Repo, GitCommandError
+import os
+
+repo_url = 'https://github.com/venkatesh-reddy-prog/Template_Repo.git'
+clone_dir = 'Template_Repo'
+
+def clone_repo(repo_url, clone_dir):
+    if not os.path.exists(clone_dir):
+        print(f"Cloning repository from {repo_url} into {clone_dir}...")
+        try:
+            Repo.clone_from(repo_url, clone_dir, depth=1)  # Shallow clone
+            print("Repository cloned successfully.")
+        except GitCommandError as e:
+            print(f"Error cloning repository: {e}")
+            return False
+    else:
+        print(f"Repository already cloned in {clone_dir}.")
+    return True
+
+if __name__ == "__main__":
+    clone_repo(repo_url, clone_dir)
